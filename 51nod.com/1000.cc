25c03785cec2ca08569c9ec8aeb00ed8873b9a08
gogdizzy <gogdizzy@gmail.com>
2014-07-17 12:59:53 +0800

    modify 1000

---
+++ 51nod.com/1000.cc
@@ -0,0 +1,31 @@
+//
+//  http://www.51nod.com/onlineJudge/questionCode.html#!problemId=1000
+//  A + B
+//
+
+#ifdef  VER2
+
+#include <iostream>
+
+int main() {
+	int  a, b;
+	std::cin >> a >> b;
+	std::cout << a + b << '\n';
+	return 0;
+}
+
+#endif
+
+#ifdef  VER1
+
+#include <stdio.h>
+
+int main() {
+	int  a, b;
+	scanf( "%d%d", &a, &b );
+	printf( "%d\n", a + b );
+	return 0;
+}
+
+#endif
+
