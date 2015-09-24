c9eb1cbb427d059bd017bf068b1250fb4f4c6995
gogdizzy <gogdizzy@gmail.com>
2015-09-24 19:38:17 +0800

    mod 1008 1009

--- 51nod.com/1008.cc
+++ 51nod.com/1008.cc
@@ -1,19 +1,21 @@
-/**
-*  http://www.51nod.com/onlineJudge/questionCode.html#!problemId=1008
-*  NµÄ½×³Ë mod P
-*  Ë¼Â·£ºÓ²Ëã
+/*
+@author gogdizzy
+@date   2015-08-19
+
+@question
+	http://www.51nod.com/onlineJudge/questionCode.html#!problemId=1008
 */
 
 #include <stdio.h>
 
 int main() {
-  unsigned int  n, p, i, j;
-  unsigned long long  ans;
-  scanf( "%u%u", &n, &p );
-  ans = ( n & 1 ) ? n / 2 + 1 : 1;
-  for( i = 1, j = n; i < j; ++i, --j ) {
-    ans = i * j * ans % p;
-  }
-  printf( "%I64u\n", ans );
-  return 0;
+	unsigned int n, p, i, j;
+	unsigned long long ans;
+	scanf( "%u%u", &n, &p );
+	ans = ( n & 1 ) ? n / 2 + 1 : 1;
+	for( i = 1, j = n; i < j; ++i, --j ) {
+		ans = i * j * ans % p;
+	}
+	printf( "%I64u\n", ans );
+	return 0;
 }
